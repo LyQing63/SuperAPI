86fb61151b21276ed7353ddc3093535f7fc92bc2
LyQing63 <1827304875@qq.com>
2024-12-02 15:06:11 +0800

    完成Nacos的负载均衡

--- SuperAPI-utils/src/main/java/com/github/lyqing63/superapi/utils/utils/PasswordEncoder.java
+++ SuperAPI-utils/src/main/java/com/github/lyqing63/superapi/utils/utils/PasswordEncoder.java
@@ -1,13 +1,27 @@
 package com.github.lyqing63.superapi.utils.utils;
 
-import cn.hutool.crypto.symmetric.SymmetricCrypto;
+import cn.hutool.core.codec.Base64;
+import cn.hutool.crypto.Mode;
+import cn.hutool.crypto.Padding;
+import cn.hutool.crypto.symmetric.AES;
+
+import java.nio.charset.StandardCharsets;
 
 public class PasswordEncoder {
 
-    private static SymmetricCrypto aes = new SymmetricCrypto("AES");
+    /**
+     * 密钥
+     */
+    private static String key = "LyQing6312345612";
+    /**
+     * 初始化向量
+     */
+    private static String iv = "LyQing6312345612";
+
+    private static AES aes = new AES(Mode.CBC, Padding.PKCS5Padding, key.getBytes(StandardCharsets.UTF_8), iv.getBytes(StandardCharsets.UTF_8));
 
     public static String encode(String password) {
-        return aes.encryptHex(password);
+        return aes.encryptBase64(password);
     }
 
     public static Boolean matches(String input, String ori) {
